91f12e65d1c4a85987647e70d45fe64437700bcc
Nikhil Pisolkar <npisolka@data.cs.purdue.edu>
2022-06-02 12:08:37 -0400

    readme update and start of segment class

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # sudoku
 sudoku project
+
+new data structure?
+
+java class BOARD
+contains a 2d array of SEGMENTs
+each contains a 2d array of shorts from 0-9
+
+given a sudoku puzzle should be able to solve it
+
+
---
+++ Segment.java
@@ -0,0 +1,18 @@
+/**
+ * Nikhil Pisolkar
+ * Sudoku project
+ * 6/1/2022
+ * Segment class, 3x3 boxes, with getter and setter methods
+ */
+
+public class Segment {
+  int[][] digits; 
+  int posRow;
+  int posCol;
+
+  public Segment(short[][] nums) {
+    
+  }
+
+}
+
